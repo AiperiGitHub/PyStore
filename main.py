e0b5ec55785293838aa6eec0d22abe5b87592d16
Aiperi Edilbekova <edilbekova_aiperi@mail.ru>
2023-03-07 15:30:27 +0300

    Протестировал модельки

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from shop.model import Product
+from shop.model import Product, Category
 
-Product()
+phone = Category('Phones')
+
+iphone15 = Product('Iphone 15 black', 'Apple', 'Iphone 15', 990, 'black', 256, category=phone)
+Samsung = Product('Samsung S23', 'Samsung', 'S23', 850, 'white', 512, category=phone)
+
+print(iphone15.__dict__)
+print(iphone15.__dict__)
